5c7cbb9514bd6a369b94d3724e669d7039e37798
Kadir Korkmaz <kadir33korkmaz@gmail.com>
2022-09-26 03:05:48 +0300

    improve on card generator area

--- components/CardGeneratorPage.tsx
+++ components/CardGeneratorPage.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cardData from '../public/cardData.json';
 
 function CardGeneratorPage() {
-  return <div className=" w-full h-[600px]">CardGeneratorPage</div>;
+  const [activePage, setActivePage] = useState(1);
+
+  return (
+    <div className=" w-full ">
+      <div className="pageTotal mt-16">
+        <div className="mt-7 pt-7 mb-16">
+          <h3 className="mb-4 font-semibold text-4xl">Modal Card Generator</h3>
+          <p className="max-w-[28.75rem]">
+            Measure your return on email marketing efforts easier and faster by
+            using thebest online tools. Popupsmart is ready to help you build an
+            efficient email list!
+          </p>
+        </div>
+        <div className=" mt-7 pt-7 flex items-center gap-4">
+          <span className="font-semibold bg-gray-200 w-9 h-9 flex justify-center items-center rounded-full">
+            1
+          </span>
+          <h3 className=" text-lg font-semibold">Choose your template</h3>
+        </div>
+        <div className="cardSelect mt-8 gap-8 flex flex-wrap">
+          {cardData.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="bg-neutral-100 w-[17.25rem] h-[12.5rem] border rounded-xl border-gray-200 flex items-center justify-center"
+              >
+                <img src={item.image} alt="card" />
+              </div>
+            );
+          })}
+        </div>
+        <div className="p-[3px] mt-7 bg-neutral-100 w-[8.25rem] rounded-xl mb-7">
+          {Array.apply(0, Array(3)).map((x, i) => {
+            return (
+              <button
+                key={i}
+                onClick={() => {
+                  setActivePage(i + 1);
+                }}
+                className={`w-[42px] h-[42px] rounded-[10px] text-sm font-semibold hover:bg-gray-50  ${
+                  activePage === i + 1
+                    ? 'bg-white text-black'
+                    : 'text-neutral-500'
+                }`}
+              >
+                {i + 1}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CardGeneratorPage;
--- components/Faq.tsx
+++ components/Faq.tsx
@@ -39,7 +39,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
@@ -104,7 +104,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
@@ -167,7 +167,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
@@ -230,7 +230,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
@@ -293,7 +293,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
@@ -362,7 +362,7 @@ function Faq() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_79_9693)">
+                <g clipPath="url(#clip0_79_9693)">
                   <path
                     d="M7 11V13H17V11H7ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20Z"
                     fill="#7D4AEA"
