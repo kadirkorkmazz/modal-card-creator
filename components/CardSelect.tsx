299f02822d305e7f762e3d0bb4da68233424bb2a
Kadir Korkmaz <kadir33korkmaz@gmail.com>
2022-10-05 01:24:47 +0300

    selection blocked of non-customizable modals

--- components/CardSelect.tsx
+++ components/CardSelect.tsx
@@ -15,19 +15,37 @@ function CardSelect(props: { activePage: number }) {
         return (
           <div
             key={item.id}
-            className="bg-neutral-100 w-[17.25rem] h-[12.5rem] border rounded-xl border-gray-200 flex items-center justify-center relative group hover:cursor-pointer"
+            className={`bg-neutral-100 w-[17.25rem] h-[12.5rem] border rounded-xl border-gray-200 flex items-center justify-center relative group  ${
+              item.customizable ? 'hover:cursor-pointer' : 'cursor-not-allowed '
+            }`}
             onClick={() => {
-              setSelectedCard(item.id);
+              if (item.customizable) {
+                setSelectedCard(item.id);
+              }
             }}
           >
             <div
-              className={`absolute bg-violet-600 bg-opacity-40 w-full h-full items-center justify-center rounded-xl text-violet-600 font-sans font-semibold tracking-tight flex  group-hover:visible transition-all ${
+              className={`absolute bg-opacity-40 w-full h-full items-center justify-center rounded-xl text-violet-600 font-sans font-semibold tracking-tight flex  group-hover:visible transition-all ${
                 selectedCard === item.id ? 'visible' : 'invisible'
-              } `}
-              onClick={() => setSelectedCard(item.id)}
+              } ${item.customizable ? 'bg-violet-600' : 'bg-gray-900 '} `}
+              onClick={() => {
+                if (item.customizable) {
+                  setSelectedCard(item.id);
+                }
+              }}
             >
-              <button className="bg-white  w-[165px] h-12 rounded-xl shadow-[0px_5px_10px_2px_rgba(125, 74, 234, 0.27)]">
-                {selectedCard === item.id ? 'Selected' : 'Select Template'}
+              <button
+                className={` w-[165px] h-12 rounded-xl shadow-[0px_5px_10px_2px_rgba(125, 74, 234, 0.27)] ${
+                  !item.customizable
+                    ? 'bg-violet-500 text-gray-300 hover:cursor-not-allowed'
+                    : 'bg-white hover:cursor-pointer'
+                }`}
+              >
+                {selectedCard === item.id
+                  ? 'Selected'
+                  : item.customizable
+                  ? 'Select Template'
+                  : 'Coming Soon'}
               </button>
             </div>
             <img src={item.image} alt="card" />
